46d957a203875d083a99cf483a592fd2ecd9a9ac
dVera17 <dsvera174@gmail.com>
2023-12-19 12:48:02 -0500

    feat: :sparkles: Config files
    Config files

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,6 @@ import { get } from 'env-var';
 export const envs = {
     PORT: get('PORT_SERVER').required().asPortNumber(),
     HOST: get('HOST_SERVER').required().asString(),
+    MONGO_URL: get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
 }
