ac168a377fd6821dd05921d151aa502cd6a8e98f
dVera17 <dsvera174@gmail.com>
2023-12-19 12:47:34 -0500

    feat: :sparkles: Raise the server
    Raise the server

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import { Server } from './presentation/server';
 import { envs } from './config';
+import { AppRoutes } from './presentation/routes';
+import { MongoDatabase } from './data/mongodb';
 
 (() => {
     main();
 })()
 
 async function main() {
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
     new Server({
         port: envs.PORT,
         host: envs.HOST,
+        routes: AppRoutes.routes
     })
         .start()
 }
